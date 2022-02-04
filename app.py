89efa518a902b48191865fba60548a5befd1c0b4
Filpor <filip.porebinski@o2.pl>
2022-02-04 22:03:08 +0100

    .

--- app.py
+++ app.py
@@ -11,4 +11,27 @@ with open('waluty.csv', 'w', encoding='utf-8') as csvfile:
     csvwriter.writeheader()
     for n in rates:
         csvwriter.writerow(n)
+dolar=rates[0]
+dolar_austalijski=rates[1]
+dolar_kanadyjski=rates[2]
+
+
 app=Flask(__name__)
+@app.route('/waluty', methods=['GET',"POST"])
+def waluty():
+    if request.method=='GET':
+        return render_template("waluty.html")
+    elif request.method=='POST':
+        waluta=request.form['waluty']
+        ilosc=float(request.form['ilosc'])
+        if waluta=='USD':
+            wartosc=ilosc/dolar.get('ask')
+            wartosc=str(wartosc)
+            print(wartosc)
+            return f'Wartość w USD: {wartosc}'
+        elif waluta=='AUD':
+            wartosc=ilosc/dolar_austalijski.get('ask')
+            return f'Wartość w AUD: {wartosc}'
+        elif waluta=='CAD':
+
+    return render_template("waluty.html")
