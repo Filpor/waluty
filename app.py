add11427322210781d1f2814c4fc59b8813f5c39
Filpor <filip.porebinski@o2.pl>
2022-02-04 18:32:40 +0100

    pierwsza_czesc

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask,request, redirect, render_template
 import requests
+import csv
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
+data1=data[0]
+rates=data1["rates"]
+with open('waluty.csv', 'w', encoding='utf-8') as csvfile:
+    fieldnames = ['currency', 'code', 'bid', 'ask']
+    csvwriter = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
+    csvwriter.writeheader()
+    for n in rates:
+        csvwriter.writerow(n)
 app=Flask(__name__)
